3fd6c19a4cd4bc2d69005f0fcbc7b783f130529b
Kurovah <45458052+Kurovah@users.noreply.github.com>
2020-01-05 21:24:17 +0000

    UI stuff

---
+++ Assets/scripts/Danny/rotateStartShip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateStartShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform ship;
+    void Start()
+    {
+        ship = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ship.Rotate(0, 0.05f, 0);
+    }
+}
